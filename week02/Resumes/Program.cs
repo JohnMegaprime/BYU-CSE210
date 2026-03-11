4f78741bbe03a74280455f293f7cd179fd246df2
joão Souza <joaomegaprime@gmail.com>
2026-03-11 01:09:00 -0300

    Completed the learning activity "Abstractions" in week 2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        Resume myResume = new Resume();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        job2._company = "Samsung";
+        job2._jobTitle = "Salesman";
+        job2._startYear = 2025;
+        job2._endYear = 2026;
+
+        myResume._name = "João Gabriel";
+
+        myResume._Jobs.Add(job1);
+        myResume._Jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
