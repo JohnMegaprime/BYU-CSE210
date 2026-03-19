ead3981599da205e434e875a9b9fea68785ca896
joão Souza <joaomegaprime@gmail.com>
2026-03-18 23:44:22 -0300

    Completed w3 learning activity

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,55 @@
+
+public class Fraction
+{   
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int topNumber, int bottomNumber)
+    {
+        _top = topNumber;
+        _bottom = bottomNumber;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBotton()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
