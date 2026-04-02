302a90fc12868e93c1fc6d4817a9666963401b5e
joão Souza <joaomegaprime@gmail.com>
2026-04-01 22:55:41 -0300

    completed w05 learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("joão Gabriel", "Addition");
+
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Cleberildo", "Multiplication", "6.7", "1-7");
+
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Melyssa", "History", "The History of Brazil");
+
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
